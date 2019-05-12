a45354f04fad425221e9571a778dae7aa9ea16f7
elangreen <green.elan5@gmail.com>
2019-05-12 15:24:46 -0500

    fingers crossed

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful'
 }
